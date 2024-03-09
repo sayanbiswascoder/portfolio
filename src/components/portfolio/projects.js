29f190477e68a597448da30e6618cebc1a7c5c1d
Sayan Biswas <sayanbiswas6073@gmail.com>
2024-03-09 23:38:29 +0530

    adding a new project

--- src/components/portfolio/projects.js
+++ src/components/portfolio/projects.js
@@ -3,6 +3,7 @@ import IMG2 from '../../assets/portfolio2.png'
 import IMG3 from '../../assets/portfolio3.png'
 import IMG4 from '../../assets/portfolio4.png'
 import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.png'
 import {BsGithub} from 'react-icons/bs'
 import {SiReplit} from 'react-icons/si'
 
@@ -28,12 +29,21 @@ const projectrs = [
     },
     {
         "name":"Chat App -random chat",
-        "desc": "Here you cat with random parsom who are using the webapp at the same time.",
+        "desc": "Here you can chat with random parsom who are using the webapp at the same time.",
         "fontColor":"white",
         "sourceIcon":<SiReplit/>,
         "sourceUrl": "https://replit.com/@SayanBiswas8/chat-App",
         "url":"https://chat-app.sayanbiswas8.repl.co",
         "imgUrl":IMG3
+    },
+    {
+        "name":"Sudoku Solver",
+        "desc": "Online Sudoku Solver.",
+        "fontColor":"white",
+        "sourceIcon":<BsGithub/>,
+        "sourceUrl": "https://github.com/sayan-the-rat/sudoku-solver",
+        "url":"https://sudoku-solver-bysb.vercel.app",
+        "imgUrl":IMG4
     },
     {
         "name":"Multiplayer Online TicTacToe",
@@ -42,7 +52,7 @@ const projectrs = [
         "sourceIcon":<SiReplit/>,
         "sourceUrl": "https://replit.com/@SayanBiswas8/tictactoe",
         "url":"https://tictactoe.sayanbiswas8.repl.co",
-        "imgUrl":IMG4
+        "imgUrl":IMG5
     },
     {
         "name":"ToDo Chart",
@@ -51,7 +61,7 @@ const projectrs = [
         "sourceIcon":<BsGithub/>,
         "sourceUrl": "https://github.com/sayan-the-rat/todo",
         "url":"https://sayan-the-rat.github.io/todo/",
-        "imgUrl":IMG5
+        "imgUrl":IMG6
     }
 ]
 
